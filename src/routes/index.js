72463645795e4842922ef53428b5d828687467db
Gustavo Souza <gustavo.ifto@gmail.com>
2020-04-06 21:26:32 -0300

    Configure Root Import

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import Route from './Route';
 
-import SignIn from '../pages/SignIn';
+import SignIn from '~/pages/SignIn';
 
-import Dashboard from '../pages/Dashboard';
-import Deliverymans from '../pages/Deliverymans';
-import DeliverymanForm from '../pages/DeliverymanForm';
-import DeliveryProblems from '../pages/DeliveryProblems';
-import Recipients from '../pages/Recipients';
-import RecipientForm from '../pages/RecipientForm';
-import OrderForm from '../pages/OrderForm';
+import Dashboard from '~/pages/Dashboard';
+import Deliverymans from '~/pages/Deliverymans';
+import DeliverymanForm from '~/pages/DeliverymanForm';
+import DeliveryProblems from '~/pages/DeliveryProblems';
+import Recipients from '~/pages/Recipients';
+import RecipientForm from '~/pages/RecipientForm';
+import OrderForm from '~/pages/OrderForm';
 
 export default function Routes() {
   return (
